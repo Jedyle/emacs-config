f05840d523a93f82f6aa76069fe3b7a698c043d3
Jedyle <jeremy.lixandre@gmail.com>
2023-02-01 14:47:32 +0100

    chg: :children_crossing: add M-g d for ace delete window

--- init.el
+++ init.el
@@ -2,7 +2,6 @@
 (setq inhibit-startup-message t)
 (load-theme 'tsdh-dark)
 (global-set-key (kbd "C-z") 'undo)
-(global-set-key (kbd "M-p") 'ace-window)
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; Marking text
@@ -32,13 +31,30 @@
  ;; If there is more than one, they won't work right.
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(yaml-mode yasnippet resize-window markdown-mode terraform-mode hcl-mode dockerfile-mode multiple-cursors helm-projectile treemacs-magit treemacs-projectile projectile magit)))
+   '(direnv yaml-mode yasnippet resize-window markdown-mode terraform-mode hcl-mode dockerfile-mode multiple-cursors helm-projectile treemacs-magit treemacs-projectile projectile magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; ACE WINDOW
+(global-set-key (kbd "M-p") 'ace-window)
+(defvar aw-dispatch-alist
+  '((?d aw-delete-window "Delete Window")
+    (?m aw-swap-window "Swap Windows")
+    (?M aw-move-window "Move Window")
+    (?c aw-copy-window "Copy Window")
+    (?j aw-switch-buffer-in-window "Select Buffer")
+    (?n aw-flip-window)
+    (?u aw-switch-buffer-other-window "Switch Buffer Other Window")
+    (?c aw-split-window-fair "Split Fair Window")
+    (?v aw-split-window-vert "Split Vert Window")
+    (?b aw-split-window-horz "Split Horz Window")
+    (?o delete-other-windows "Delete Other Windows")
+    (?? aw-show-dispatch-help))
+  "List of actions for `aw-dispatch-default'.")
 
 ;; Completion
 (global-set-key (kbd "C-:") 'dabbrev-expand)
@@ -74,3 +90,5 @@
 ;; ELECTRIC PAIR MODE
 (electric-pair-mode 1)
 
+;; DIRENV MODE
+(direnv-mode)
